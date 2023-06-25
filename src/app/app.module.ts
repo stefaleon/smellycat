d1dcec857d0a3fa4e5709a1c3d64581f0f6ebfb9
stefaleon <stefanidisl@iconplatforms.com>
2023-06-25 21:19:14 +0300

    Generate the app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ContactFormComponent } from './pages/contact/contact-form/contact-form.component';
+import { ContactDataComponent } from './pages/contact/contact-data/contact-data.component';
+import { MapComponent } from './pages/contact/contact-data/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent,
+    ContactComponent,
+    ContactFormComponent,
+    ContactDataComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/contact/contact-data/contact-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-data',
+  templateUrl: './contact-data.component.html',
+  styleUrls: ['./contact-data.component.css']
+})
+export class ContactDataComponent {
+
+}
