2ca8fb563cdfe1c28bb4d9dcd64c7f8d51682517
stefaleon <stefanidisl@iconplatforms.com>
2023-06-26 09:53:14 +0300

    Configure the footer component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,21 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
-	"./node_modules/tw-elements/dist/js/**/*.js"
+    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        us: "340px",
+        xs: "440px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
   },
   darkMode: "class",
-  plugins: [require("tw-elements/dist/plugin.cjs")]
-}
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+};
