608eace9af047ff6377ed43cfcdd851aa7af3af2
stefaleon <stefanidisl@iconplatforms.com>
2023-06-30 19:54:00 +0300

    Configure the contact form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
@@ -10,6 +10,7 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { ContactFormComponent } from './pages/contact/contact-form/contact-form.component';
 import { ContactDataComponent } from './pages/contact/contact-data/contact-data.component';
 import { MapComponent } from './pages/contact/contact-data/map/map.component';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { MapComponent } from './pages/contact/contact-data/map/map.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/contact/contact-form/contact-form.component.ts
+++ src/app/pages/contact/contact-form/contact-form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  styleUrls: ['./contact-form.component.css'],
 })
-export class ContactFormComponent {
+export class ContactFormComponent implements OnInit {
+  contactForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      city: ['', Validators.required],
+      postalCode: ['', Validators.required],
+      address: ['', Validators.required],
+      message: [''],
+      termsAndConditions: [false, Validators.requiredTrue],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      // Process the form data here
+      console.log(this.contactForm.value);
+      // Clear form
+    }
+  }
 }
